8dc82a5f769d03dc9ec943fe209418c579baf9fd
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-21 00:38:19 +0430

    linters errors solved

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import './style.css';
 
-const recentScore = document.getElementsById('recent')
+// const recentScore = document.getElementsById('recent')
 const leaderboardForm = document.getElementsById('add-your-score')
 
 leaderboardForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   const person = document.getElementById('input-name');
   const score = document.getElementById('input-score');
-  const gameFormData = {
-    person: person.value,
-    score: parseInt(score.value, 10),
-  };
+  // const leaderboardFormData = {
+  //   person: person.value,
+  //   score: parseInt(score.value, 10),
+  // };
 });
