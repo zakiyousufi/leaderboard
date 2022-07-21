0b35f12581de601b841ffb0021ebc77c219aa485
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-21 23:06:26 +0430

    Add rule to the project so it gives user name and score that typed on the input boxes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ const leaderboardForm = document.querySelector('.add-score');
 
 leaderboardForm.addEventListener('submit', async (e) => {
   e.preventDefault();
-  const person = document.getElementById('input-name');
+  const user = document.getElementById('input-name');
   const score = document.getElementById('input-score');
   const leaderboardFormData = {
-    person: person.value,
+    user: user.value,
     score: parseInt(score.value, 10),
   };
   const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${playId}/scores/`, {
@@ -19,7 +19,7 @@ leaderboardForm.addEventListener('submit', async (e) => {
     },
   });
   const result = await response.json();
-  person.value = '';
+  user.value = '';
   score.value = '';
   return result;
 });
@@ -34,15 +34,15 @@ const getInfo = async () => {
 const addData = async () => {
   const players = await getInfo();
   const allPlayerPart = document.getElementById('recent');
-  players.result.forEach((player) => {
-    const playerInfo = `<div>${player.player}: ${player.score}</div>`;
+  players.result.forEach((user) => {
+    const playerInfo = `<div>${user.user}: ${user.score}</div>`;
     allPlayerPart.insertAdjacentHTML('beforeend', playerInfo);
   });
 };
 
 const refreshData = async () => {
   const allUsersPart = document.getElementById('scores');
-  allUsersPart.innerHTML = '';
+  allUsersPart.innerHTML = 'playerInfo';
   await addData();
 };
 
