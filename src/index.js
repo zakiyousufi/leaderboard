f4769c0649674ef5ea22ceeac1410852e09ffc5b
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-21 15:16:45 +0430

    build the deploy page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+// const recentScore = document.getElementsById('recent')
+const leaderboardForm = document.getElementsById('add-your-score')
+
+leaderboardForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const person = document.getElementById('input-name');
+  const score = document.getElementById('input-score');
+  // const leaderboardFormData = {
+  //   person: person.value,
+  //   score: parseInt(score.value, 10),
+  // };
+});
