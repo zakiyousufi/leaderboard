c378e10ba14e3955d55546029bf2fdd88e02b0e3
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-21 16:01:11 +0430

    Add game ID

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import './style.css';
 
-// const recentScore = document.getElementsById('recent')
+const recentScore = document.getElementsById('recent')
+const playID = 'CsVjrIhDWuIKDeZ1pqQ'
 const leaderboardForm = document.getElementsById('add-your-score')
 
 leaderboardForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   const person = document.getElementById('input-name');
   const score = document.getElementById('input-score');
-  // const leaderboardFormData = {
-  //   person: person.value,
-  //   score: parseInt(score.value, 10),
-  // };
+  const leaderboardFormData = {
+    person: person.value,
+    score: parseInt(score.value, 10),
+  };
+
 });
